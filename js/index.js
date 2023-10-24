c1f537977bac0e69aef6ce1a4a751c74c2709786
Junior Rodrigues <junior.josuerodrigues@gmail.com>
2023-10-24 18:50:49 -0300

    validação javascript

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
-let botao1 = document.getElementById("botao1")
+const form = document.getElementById('form')
+const campos = document.querySelectorAll('.required')
+const spans = document.querySelectorAll('.span-required')
+const emailRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-botao1.onclick = () => {
-    alert('Obrigado por assinar nossa Newsletter!');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    nameValidate();
+    emailValidate();
+})
+
+function removeError(index){
+    campos[index].style.border = '';
+    spans[index].style.display = 'none';
 }
+
+
+function setError(index){
+    campos[index].style.border = '1px solid #e63636';
+    spans[index].style.display = 'block';
+}
+
+function nameValidate(){
+    if(campos[0].value.length < 3) 
+    {
+        setError(0);
+    }
+    else {
+        removeError(0);
+    }
+}
+
+function emailValidate(){
+    if(!emailRegex.test(campos[1].value))
+    {
+        setError(1);
+    }
+    else {
+        removeError(1);
+    }  
+}
+
